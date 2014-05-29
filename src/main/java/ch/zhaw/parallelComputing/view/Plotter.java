45a13433f3321f2286746ce814fe34c74bc82f08
mxmo0rhuhn <max-schrimpf@hotmail.com>
2014-05-29 02:32:37 +0200

    fancy Plot dialog

--- src/main/java/ch/zhaw/parallelComputing/view/Plotter.java
+++ src/main/java/ch/zhaw/parallelComputing/view/Plotter.java
@@ -1,73 +1,134 @@
 package ch.zhaw.parallelComputing.view;
 
-import com.googlecode.charts4j.*;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.axis.Axis;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.time.Month;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
 import org.jfree.data.xy.XYDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
-import org.jfree.ui.ApplicationFrame;
-import org.jfree.ui.RefineryUtilities;
 
-import static com.googlecode.charts4j.Color.*;
-
+import javax.swing.*;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.logging.Logger;
 
 /**
  * Created by Max Schrimpf
  */
 
-public class Plotter extends ApplicationFrame {
+public class Plotter {
 
     public static void main(String[] args) {
-        Plotter demo = new Plotter("Time Series Demo 1");
-        demo.pack();
-        RefineryUtilities.centerFrameOnScreen(demo);
-        demo.setVisible(true);
+        XYDataset dataset1 = createDataset();
+        XYDataset dataset2 = createDataset2();
+        new Plotter().plot("Test", dataset1, dataset2);
     }
 
-    public Plotter(String title) {
-        super(title);
-        JFreeChart chart = example1();
-        ChartPanel chartPanel = new ChartPanel(chart);
-        chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
-        chartPanel.setMouseZoomable(true, false);
-        setContentPane(chartPanel);
-    }
+    private static void plot(String title, XYDataset dataset1, XYDataset dataset2) {
 
-    private JFreeChart example1() {
-        // Create a simple XY chart
-        XYSeries series = new XYSeries("XYGraph");
-        series.add(1, 1);
-        series.add(1, 2);
-        series.add(2, 1);
-        series.add(3, 9);
-        series.add(4, 10);
-        // Add the series to your data set
-        XYSeriesCollection dataset = new XYSeriesCollection();
-        dataset.addSeries(series);
-        // Generate the graph
-        JFreeChart chart = ChartFactory.createXYLineChart(
-                "XY Chart", // Title
-                "x-axis", // x-axis Label
-                "y-axis", // y-axis Label
-                dataset, // Dataset
-                PlotOrientation.VERTICAL, // Plot Orientation
-                true, // Show Legend
-                true, // Use tooltips
-                false // Configure chart to generate URLs?
+        JFreeChart chart = ChartFactory.createTimeSeriesChart(
+                "Comparison",           // title
+                "Date",                 // x-axis label
+                "Input Value",          // y-axis label
+                dataset1,               // data
+                true,                   // create legend?
+                true,                   // generate tooltips?
+                false                   // generate URLs?
         );
-        try {
-            ChartUtilities.saveChartAsJPEG(new File("test.jpg"), chart, 500, 300);
-        } catch (IOException e) {
-            System.err.println("Problem occurred creating chart.");
+
+        XYPlot plot = chart.getXYPlot();
+        plot.setDataset(1, dataset2);
+
+        Axis axis1 = plot.getRangeAxis(0);
+        axis1.setLabelPaint(Color.red);
+
+        NumberAxis axis2 = new NumberAxis("Twitter");
+        axis2.setLabelPaint(Color.blue);
+        plot.setRangeAxis(1, axis2);
+
+        XYLineAndShapeRenderer renderer =  new XYLineAndShapeRenderer();
+        renderer.setSeriesPaint(0, Color.blue);
+        renderer.setBaseShapesVisible(false);
+        plot.setRenderer(1, renderer);
+
+
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(1024, 768));
+        chartPanel.setMouseZoomable(true, false);
+
+        String[] options = {"Print" , "OK"};
+        int response = JOptionPane.showOptionDialog(
+                null                       // Center in window.
+                , chartPanel
+                , title                      // Title in titlebar
+                , JOptionPane.YES_NO_OPTION  // Option type
+                , JOptionPane.PLAIN_MESSAGE  // messageType
+                , null                       // Icon (none)
+                , options                     // Button text as above.
+                , "OK"                        // Default button
+        );
+
+        if(response == 0) {
+            try {
+                ChartUtilities.saveChartAsPNG(new File(title + ".png"), chart, 1920, 1080);
+            } catch (IOException e) {
+                System.err.println("Problem occurred creating chart.");
+            }
         }
-        return chart;
+    }
+
+    private static XYDataset createDataset() {
+        TimeSeries s1 = new TimeSeries("L&G European Index Trust", Month.class);
+        TimeSeriesCollection dataset = new TimeSeriesCollection();
+        s1.add(new Month(2, 2001), 181.8);
+        s1.add(new Month(3, 2001), 167.3);
+        s1.add(new Month(4, 2001), 153.8);
+        s1.add(new Month(5, 2001), 167.6);
+        s1.add(new Month(6, 2001), 158.8);
+        s1.add(new Month(7, 2001), 148.3);
+        s1.add(new Month(8, 2001), 153.9);
+        s1.add(new Month(9, 2001), 142.7);
+        s1.add(new Month(10, 2001), 123.2);
+        s1.add(new Month(11, 2001), 131.8);
+        s1.add(new Month(12, 2001), 139.6);
+        s1.add(new Month(1, 2002), 142.9);
+        s1.add(new Month(2, 2002), 138.7);
+        s1.add(new Month(3, 2002), 137.3);
+        s1.add(new Month(4, 2002), 143.9);
+        s1.add(new Month(5, 2002), 139.8);
+        s1.add(new Month(6, 2002), 137.0);
+        s1.add(new Month(7, 2002), 132.8);
+        dataset.addSeries(s1);
+        return dataset;
+    }
+    private static XYDataset createDataset2() {
+        TimeSeries s2 = new TimeSeries("Twitter" , Month.class);
+        TimeSeriesCollection dataset = new TimeSeriesCollection();
+        s2.add(new Month(2, 2001), 129.6);
+        s2.add(new Month(3, 2001), 123.2);
+        s2.add(new Month(4, 2001), 117.2);
+        s2.add(new Month(5, 2001), 124.1);
+        s2.add(new Month(6, 2001), 122.6);
+        s2.add(new Month(7, 2001), 119.2);
+        s2.add(new Month(8, 2001), 116.5);
+        s2.add(new Month(9, 2001), 112.7);
+        s2.add(new Month(10, 2001), 101.5);
+        s2.add(new Month(11, 2001), 106.1);
+        s2.add(new Month(12, 2001), 110.3);
+        s2.add(new Month(1, 2002), 111.7);
+        s2.add(new Month(2, 2002), 111.0);
+        s2.add(new Month(3, 2002), 109.6);
+        s2.add(new Month(4, 2002), 113.2);
+        s2.add(new Month(5, 2002), 111.6);
+        s2.add(new Month(6, 2002), 108.8);
+        s2.add(new Month(7, 2002), 101.6);
+        dataset.addSeries(s2);
+        return dataset;
     }
 }
