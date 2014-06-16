9ac3a5252146aa3ed38b912f86cc8cfe4d316c61
mxmo0rhuhn <max-schrimpf@hotmail.com>
2014-06-16 14:28:41 +0200

    path was wrong

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
-java -Xmx4g -Djava.util.logging.config.file=logging.properties -jar seminarParallelComputing_Max_Schrimpf.jar
+java -Xmx4g -Djava.util.logging.config.file=logging.properties -jar target/parallelComputing-0.1-SNAPSHOT-jar-with-dependencies.jar
 @echo off
 set /p id="Hit Enter to finish" %=%
